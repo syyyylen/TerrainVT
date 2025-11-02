aa97a4c1346915fc99f9fad49a373fe1135e713b
Jules Vannier <julesvannier44@gmail.com>
2025-11-02 13:51:02 +0100

    Move renderer into Terrain App

--- Terrain/TerrainApp.h
+++ Terrain/TerrainApp.h
@@ -5,7 +5,11 @@
 #include <iomanip>
 #include <ctime>
 
-#include "D3D12Renderer.h"
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include "Include/d3dx12.h"
+
+#define FRAMES_IN_FLIGHT 3
 
 class TerrainApp 
 {
@@ -18,6 +22,8 @@ public:
 	void Quit();
 
 private:
+
+	void WaitForPreviousFrame();
 
 	struct Vec2
 	{
@@ -47,5 +53,16 @@ private:
 	float m_lastTime = 0.0f;
 	float m_timeElapsed = 0.0f;
 
-	std::unique_ptr<D3D12Renderer> m_renderer = nullptr;
+	ID3D12Device* m_device = nullptr;
+	IDXGISwapChain3* m_swapChain = nullptr;
+	ID3D12CommandQueue* m_commandQueue = nullptr;
+	ID3D12DescriptorHeap* m_rtvDescriptorHeap = nullptr;
+	ID3D12Resource* m_renderTargets[FRAMES_IN_FLIGHT] = {};
+	ID3D12CommandAllocator* m_commandAllocators[FRAMES_IN_FLIGHT] = {};
+	ID3D12GraphicsCommandList* m_commandList = nullptr;
+	ID3D12Fence* m_fences[FRAMES_IN_FLIGHT] = {};
+	HANDLE m_fenceEvent = nullptr;
+	UINT64 m_fenceValues[FRAMES_IN_FLIGHT] = {};
+	int m_frameIndex = 0;
+	int m_rtvDescriptorSize = 0;
 };
