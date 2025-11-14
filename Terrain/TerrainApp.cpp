9075c5286a411bd8adcaa25440b5df259a688f01
Jules Vannier <julesvannier44@gmail.com>
2025-11-14 15:27:00 +0100

    Point Sampler instead of Linear

--- Terrain/TerrainApp.cpp
+++ Terrain/TerrainApp.cpp
@@ -395,7 +395,7 @@ TerrainApp::TerrainApp()
 	rootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
 
 	D3D12_STATIC_SAMPLER_DESC sampler = {};
-	sampler.Filter = D3D12_FILTER_MIN_MAG_MIP_LINEAR;
+	sampler.Filter = D3D12_FILTER_MIN_MAG_MIP_POINT;
 	sampler.AddressU = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
 	sampler.AddressV = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
 	sampler.AddressW = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
