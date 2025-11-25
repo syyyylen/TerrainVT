0ed65ef76e706ae65a737c3a686c399dc40c14bf
Jules Vannier <julesvannier44@gmail.com>
2025-11-25 14:52:39 +0100

    VT going up in mip levels until we find one

--- Terrain/Shaders/Pixel.hlsl
+++ Terrain/Shaders/Pixel.hlsl
@@ -28,7 +28,7 @@ cbuffer ConstantBuffer : register(b0)
 
 float4 main(DSOutput input) : SV_TARGET
 {
-    float3 finalColor = float3(1.0f, 0.0f, 1.0f);
+    float3 finalColor = float3(0.0f, 0.0f, 0.0f);
 
     float2 dx = ddx(input.uv * vt_texture_size);
     float2 dy = ddy(input.uv * vt_texture_size);
@@ -39,32 +39,35 @@ float4 main(DSOutput input) : SV_TARGET
     mip = clamp(mip, 0.0f, maxMipLevel);
     mip = floor(mip); // from floating value to round mip level
     
-    float2 rqPx = floor(input.uv * vt_texture_size) / exp2(mip);
-    float2 rqPage = floor(rqPx / vt_texture_page_size);
-    float pagetableSize = (vt_texture_size / exp2(mip)) / vt_texture_page_size;
-    float2 rqPageUV = rqPage / pagetableSize;
-
-    float4 pagetableSample = vtPagetable.SampleLevel(s1, rqPageUV, mip);
-    if (pagetableSample.a > 0.0f)
+    for (int i = mip; i <= maxMipLevel; i++) // if we don't find anything in the pagetable at mip N, we continue with mip N+1 etc...
     {
-        // page physical address (tile index * tile size = pixel coords)
-        float2 pageAdress = pagetableSample.rg * 255.0f * vt_texture_page_size;
+        float2 rqPx = floor(input.uv * vt_texture_size) / exp2(i);
+        float2 rqPage = floor(rqPx / vt_texture_page_size);
+        float pagetableSize = (vt_texture_size / exp2(i)) / vt_texture_page_size;
+        float2 rqPageUV = rqPage / pagetableSize;
 
-        float2 rest = frac(rqPx / vt_texture_page_size);
-        rest = rest * vt_texture_page_size;
-
-        float2 pxAdress = pageAdress + rest; // px physical adress on main memory texture
-
-        float2 pxUVcoords = pxAdress / vt_main_memory_texture_size; // back to 0-1 range
-
-        float4 vTexColor = vtTexture.Sample(s1, pxUVcoords);
-
-        if (vTexColor.a > 0.0f)
+        float4 pagetableSample = vtPagetable.SampleLevel(s1, rqPageUV, i);
+        if (pagetableSample.a > 0.0f)
         {
-            finalColor = vTexColor.rgb;
+            // page physical address (tile index * tile size = pixel coords)
+            float2 pageAdress = pagetableSample.rg * 255.0f * vt_texture_page_size;
+
+            float2 rest = frac(rqPx / vt_texture_page_size);
+            rest = rest * vt_texture_page_size;
+
+            float2 pxAdress = pageAdress + rest; // px physical adress on main memory texture
+
+            float2 pxUVcoords = pxAdress / vt_main_memory_texture_size; // back to 0-1 range
+
+            float4 vTexColor = vtTexture.Sample(s1, pxUVcoords);
+            if (vTexColor.a > 0.0f)
+            {
+                finalColor = vTexColor.rgb;
+                break;
+            }
         }
     }
-
+    
     return float4(finalColor, 1.0f);
 
     // return float4(input.uv, 0.0f, 1.0f);
